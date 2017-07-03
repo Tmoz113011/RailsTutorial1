7d5d393f919af7319a6f7b17a4428f21f3d28618
DoTruong <tmoz113011@gmail.com>
2017-07-03 11:47:35 +0700

    Finish chap 14

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,18 @@ Rails.application.routes.draw do
     get "/about", to: "static_pages#about"
     get "/contact", to: "static_pages#contact"
     get "/signup", to: "users#new"
-    post "/signup",  to: "users#create"
+    post "/signup", to: "users#create"
     get "/login", to: "sessions#new"
-    post "/login",   to: "sessions#create"
-    delete "/logout",  to: "sessions#destroy"
-    resources :users
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+    resources :users do
+      member do
+        resources :relationships, only: [:index, :create]
+      end
+    end
   end
   resources :account_activations, only: :edit
   resources :password_resets, except: [:index, :destroy, :show]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: :destroy
 end
