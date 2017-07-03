dc45a41b8af8ad6b16372662953010a8c2977ed4
DoTruong <tmoz113011@gmail.com>
2017-07-03 11:47:35 +0700

    Finish chap 9

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_reader :remember_token
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@(?:[A-Z0-9-]+\.)+[A-Z]+\z/i
 
   validates :name, presence: true,
@@ -8,16 +9,11 @@ class User < ApplicationRecord
     format: {with: VALID_EMAIL_REGEX},
     uniqueness: {case_sensitive: false}
   validates :password, presence: true,
-    length: {minimum: Settings.validates.password.minium}
+    length: {minimum: Settings.validates.password.minium}, allow_nil: true
 
   has_secure_password
 
   before_save :email_downcase
-
-  private
-  def email_downcase
-    self.email = email.downcase
-  end
 
   class << self
     def digest string
@@ -28,5 +24,28 @@ class User < ApplicationRecord
       end
       BCrypt::Password.create string, cost: cost
     end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    @remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.blank?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attributes remember_digest: nil
+  end
+
+  private
+  def email_downcase
+    self.email = email.downcase
   end
 end
